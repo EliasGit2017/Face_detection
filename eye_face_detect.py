ec50b7494fd8ccd56783907a98717255e5962058
Elias Bendjaballah <34818818+EliasGit2017@users.noreply.github.com>
2020-06-15 03:01:06 +0200

    Update eye_face_detect.py

--- eye_face_detect.py
+++ eye_face_detect.py
@@ -3,8 +3,8 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('C:/Users/azwbd/OneDrive/Bureau/TMP/Haar_Cascade/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:/Users/azwbd/OneDrive/Bureau/TMP/Haar_Cascade/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('./haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 
